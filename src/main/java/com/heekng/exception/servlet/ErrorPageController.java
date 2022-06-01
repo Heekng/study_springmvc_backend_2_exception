224825e46dc8e075cbd2eecd3aa7d74eb694579a
heekng <rhkd1769@gmail.com>
2022-06-01 15:16:51 +0900

    오류페이지 작동원리

--- src/main/java/com/heekng/exception/servlet/ErrorPageController.java
+++ src/main/java/com/heekng/exception/servlet/ErrorPageController.java
@@ -2,7 +2,6 @@ package com.heekng.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,15 +11,28 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class ErrorPageController {
 
+    // RequestDispatcher
+    public static final String ERROR_EXCEPTION = "javax.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type";
+
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 404");
+        printErrorInfo(request);
         return "error-page/404";
     }
 
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 500");
+        printErrorInfo(request);
         return "error-page/500";
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+
+        log.info("dispatchType={}", request.getDispatcherType());
     }
 }
